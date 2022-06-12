f83b5ddbafb6f3851444f4be0a10b563cc556cee
reysong <576986481@qq.com>
2022-06-12 14:31:58 +0800

    fix(compiler): fix type error

--- packages/compiler/index.js
+++ packages/compiler/index.js
@@ -1,3 +1,3 @@
 export { tokenize, parse, dump, traverseNode } from "./parse.js";
-export { transform, transformRoot, transformElement, transformText } from "./transforme.js";
+export { transform, transformRoot, transformElement, transformText } from "./transform.js";
 export { generate } from "./generate.js";
